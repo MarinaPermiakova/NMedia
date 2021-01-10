ea83525a0e80e52e7146dbb69948d1a0563153ed
Marina Permiakova <marina.mk.glazov@gmail.com>
2021-01-10 22:11:05 +0400

    CRUD

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -1,6 +1,10 @@
 package ru.netology.nmedia
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.observe
@@ -14,21 +18,82 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val viewModel: PostViewModel by viewModels()
-        val adapter = PostAdapter(
-            {
-                viewModel.likeById(it.id)
-            },
-            {
-                viewModel.shared(it.id)
-            },
-            {
-                viewModel.viewed(it.id)
+        val adapter = PostAdapter(object : OnInteractionListener {
+            override fun onEdit(post: Post) {
+                viewModel.edit(post)
             }
-        )
+
+            override fun onLike(post: Post) {
+                viewModel.likeById(post.id)
+            }
+
+            override fun onRemove(post: Post) {
+                viewModel.removeById(post.id)
+            }
+
+            override fun onShare(post: Post) {
+                viewModel.shared(post.id)
+            }
+
+            override fun onView(post: Post) {
+                viewModel.viewed(post.id)
+            }
+        })
+
+        val textWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun afterTextChanged(s: Editable?) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (s != null) binding.group.visibility = View.VISIBLE
+            }
+        }
+
 
         binding.list.adapter = adapter
         viewModel.data.observe(this) { posts ->
             adapter.submitList(posts)
+        }
+
+        viewModel.edited.observe(this) { post ->
+            if (post.id == 0L) {
+                binding.content.addTextChangedListener(textWatcher)
+                return@observe
+            }
+            with(binding.content) {
+                requestFocus()
+                setText(post.content)
+                binding.group.visibility = View.VISIBLE
+            }
+        }
+
+        binding.save.setOnClickListener {
+
+            with(binding.content) {
+                if (text.isNullOrBlank()) {
+                    Toast.makeText(
+                        this@MainActivity,
+                        context.getString(R.string.error_empty_content),
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    return@setOnClickListener
+                }
+                viewModel.changeContent(text.toString())
+                viewModel.save()
+                setText("")
+                clearFocus()
+                AndroidUtils.hideKeyboard(this)
+            }
+            binding.group.visibility = View.GONE
+        }
+
+        binding.cancel.setOnClickListener {
+
+            with(binding.content) {
+                setText("")
+                clearFocus()
+                AndroidUtils.hideKeyboard(this)
+            }
+            binding.group.visibility = View.GONE
         }
     }
 }
